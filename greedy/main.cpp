061920a5340823e6301a0875f3e1b8015367e1e6
Alexandre Barbosa <alexandreab@live.com>
2014-10-14 21:20:45 -0300

    Fixing compare solution with debug messages

--- greedy/main.cpp
+++ greedy/main.cpp
@@ -4,7 +4,7 @@
 #include "Element.hpp"
 #include "Group.hpp"
 
-#define ELEMENT_SIZE 100
+#define ELEMENT_SIZE 10
 
 using namespace std;
 
@@ -28,7 +28,7 @@ vector<Element> solucao(vector<Element> vetor)
 			if(
 					((a > x) && (a < y )) ||
 					((b > x) && (b < y )) ||
-					((a < x) && (b > y )))
+					((a <= x) && (b >= y )))
 			{
 				is_solution=false;
 				break;
@@ -43,12 +43,24 @@ vector<Element> solucao(vector<Element> vetor)
 	return solution;
 }
 
+void imprime(vector<Element> result)
+{
+	for(vector<Element>::iterator it=result.begin(); it!= result.end(); it++)
+	{
+		cout << it->to_string() << endl;
+	}
+	cout << "--------" << endl << endl;
+}
+
+
 int main(void)
 {
 	vector<Element> result;
 	srand (time(NULL));
 
 	vector<Element> elements;
+	vector<Element> backup;
+
 	Group grupo =  Group();
 	for(int i=0;i<ELEMENT_SIZE;i++)
 	{
@@ -66,6 +78,13 @@ int main(void)
 		grupo.push_back(element);
 	}
 
+
+
+	grupo.sort_elements("fim",1);
+	backup = grupo.get_elements();
+	cout << "Vetor inicial:" << endl << endl;
+	imprime(backup);
+
 	int iniciomaior,iniciomenor,fimmaior,fimmenor,duracaomaior,duracaomenor;
 
 	//Ordenando por um criterio em ordem decrescente
@@ -73,36 +92,45 @@ int main(void)
 	grupo.sort_elements("inicio",1);
 	result = solucao(grupo.get_elements());
 	iniciomenor = result.size();
+	cout << endl << "inicio menor:" << endl << endl;
+	imprime(result);
 
 	grupo.sort_elements("inicio",-1);
 	result = solucao(grupo.get_elements());
 	iniciomaior = result.size();
+	cout << endl << "inicio maior:" << endl << endl;
+	imprime(result);
 
 	grupo.sort_elements("fim",1);
 	result = solucao(grupo.get_elements());
 	fimmenor = result.size();
+	cout << endl << "Fim menor:" << endl << endl;
+	imprime(result);
 
 	grupo.sort_elements("fim",-1);
 	result = solucao(grupo.get_elements());
 	fimmaior = result.size();
+	cout << endl << "Fim maior:" << endl << endl;
+	imprime(result);
 
 	grupo.sort_elements("duracao",1);
 	result = solucao(grupo.get_elements());
 	duracaomenor = result.size();
+	cout << endl << "duracao menor:" << endl << endl;
+	imprime(result);
 
 	grupo.sort_elements("duracao",-1);
 	result = solucao(grupo.get_elements());
 	duracaomaior = result.size();
+	cout << endl << "duracao maior:" << endl << endl;
+	imprime(result);
 
-	
 	cout << "Inicio menor: " << iniciomenor << endl;
 	cout << "Inicio maior: " << iniciomaior << endl;
 	cout << "Fim menor: " << fimmenor << endl;
 	cout << "Fim maior: " << fimmaior << endl;
 	cout << "Duracao menor: " << duracaomenor << endl;
 	cout << "Duracao maior: " << duracaomaior << endl;
-
-
 
 
 	for(vector<Element>::iterator it = result.begin(); it!= result.end(); it++)
